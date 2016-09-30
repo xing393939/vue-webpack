4a69c1f57205d9f6202258ac8ea3049af749c3ad
guest <guest@guest.com>
2016-09-30 10:31:27 +0800

    init

---
+++ src/components/ItemView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="item-view">
+    item
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus">
+@import "../variables.styl"
+
+.item-view
+  .item
+    padding-left 0
+    margin-bottom 30px
+    .index
+      display none
+  .poll-options
+    margin-left 30px
+    margin-bottom 40px
+    li
+      margin 12px 0
+    p
+      margin 8px 0
+    .subtext
+      color $gray
+      font-size 11px
+  .itemtext
+    color $gray
+    margin-top 0
+    margin-bottom 30px
+  .itemtext p
+    margin 10px 0
+</style>
